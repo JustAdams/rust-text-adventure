fd78547940b27e52219fff773f55114369b5e1e0
Justin Adams <AdamsJustin@protonmail.com>
2023-09-23 21:57:42 -0500

    Update commands.rs
    Convert direction input into enum version

--- src/interaction/commands.rs
+++ src/interaction/commands.rs
@@ -14,6 +14,7 @@ pub enum Direction {
     WEST,
     UP,
     DOWN,
+    UNDEFINED,
 }
 
 pub fn get_command(s: &str) -> Action {
@@ -25,5 +26,20 @@ pub fn get_command(s: &str) -> Action {
         _ => Action::UNDEFINED,
     };
 
-    return command;
+    command
+}
+
+// convert input direction into the enum for standardization
+pub fn get_direction(s: &str) -> Direction {
+    let direction = match s.trim().to_lowercase().as_str() {
+        "north" => Direction::NORTH,
+        "south" => Direction::SOUTH,
+        "east" => Direction::EAST,
+        "west" => Direction::WEST,
+        "up" => Direction::UP,
+        "down" => Direction::DOWN,
+        _ => Direction::UNDEFINED,
+    };
+
+    direction
 }
